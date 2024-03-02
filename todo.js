067b852d58aafb7eb40181927c28816a2439f9f3
Alperen Akarslan <alperenakarslan@Alperens-Laptop.local>
2024-03-02 14:16:26 +0300

    finish

--- todo.js
+++ todo.js
@@ -13,10 +13,63 @@ eventListeners();
 function eventListeners() { // Tüm event listenersler
     form.addEventListener("submit", addTodo);
     document.addEventListener("DOMContentLoaded", loadAllTodosToUI);
+    secondCardBody.addEventListener("click", deleteTodo);
+    filter.addEventListener("keyup", filterTodos);
+    clearButton.addEventListener("click", clearAllTodos);
 }
-function loadAllTodosToUI(){
+
+function clearAllTodos(e) {
+    if (confirm("Tüm todoları silmek istediğinizden emin misiniz?")) {
+        // arayüzden todoları temizleme
+        // todoList.innerHTML = ""; // yavas yontem sarmadı
+
+        while (todoList.firstElementChild != null) {
+            todoList.removeChild(todoList.firstElementChild);
+        };
+        localStorage.removeItem("todos");
+    }
+
+}
+
+function filterTodos(e) {
+    const filterValue = e.target.value.toLowerCase();
+    const listItems = document.querySelectorAll(".list-group-item");
+
+    listItems.forEach(function (listItem) {
+        const text = listItem.textContent.toLowerCase();
+        if (text.indexOf(filterValue) === -1) {
+            // bulamadı
+            listItem.setAttribute("style", "display: none !important");
+        }
+        else {
+            listItem.setAttribute("style", "display: block");
+        }
+    });
+}
+
+function deleteTodo(e) {
+    if (e.target.className === "fa fa-remove") {
+        e.target.parentElement.parentElement.remove();
+        deleteTodoFromStorage(e.target.parentElement.parentElement.textContent);
+        showAlert("warning", "Todo silindi.");
+    }
+}
+
+function deleteTodoFromStorage(deletetodo) {
     let todos = getTodosFromStorage();
-    todos.forEach(function(todo){
+
+    todos.forEach(function (todo, index) {
+        if (todo === deletetodo) {
+            todos.splice(index, 1); // arrayden değer silinir.
+        }
+    });
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function loadAllTodosToUI() {
+    let todos = getTodosFromStorage();
+    todos.forEach(function (todo) {
         addTodoToUI(todo);
     });
 }
@@ -39,10 +92,10 @@ function addTodo(e) {
     e.preventDefault();
 }
 
-function getTodosFromStorage(){ // storageden todoları almak.
+function getTodosFromStorage() { // storageden todoları almak.
     let todos;
 
-    if(localStorage.getItem("todos") === null){
+    if (localStorage.getItem("todos") === null) {
         todos = [];
     }
     else {
@@ -51,7 +104,7 @@ function getTodosFromStorage(){ // storageden todoları almak.
     return todos;
 }
 
-function addTodoToStorage(newTodo){
+function addTodoToStorage(newTodo) {
     let todos = getTodosFromStorage();
 
     todos.push(newTodo);
